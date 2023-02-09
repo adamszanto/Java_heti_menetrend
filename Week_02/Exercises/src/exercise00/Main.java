e93f4c82043f470051589dc659cf0cb05fa17e54
Adam <adam.szanto1@gmail.com>
2023-02-09 16:11:57 +0100

    2022.02.09.

--- Week_02/Exercises/src/exercise00/Main.java
+++ Week_02/Exercises/src/exercise00/Main.java
@@ -3,10 +3,10 @@ package exercise00;
 // Generate
 public class Main {
     public static void main(String[] args) {
-        System.out.println(getRandom(55,1920));
-        System.out.println(getRandom(55,1920));
-        System.out.println(getRandom(55,1920));
-        System.out.println(getRandom(55,1920));
+        System.out.println(getRandom(55, 1920));
+        System.out.println(getRandom(55, 1920));
+        System.out.println(getRandom(55, 1920));
+        System.out.println(getRandom(55, 1920));
     }
 
     public static long getRandom(int min, int max) {
--- Week_02/Exercises/src/exercise01/Main.java
+++ Week_02/Exercises/src/exercise01/Main.java
@@ -1,8 +1,14 @@
 package exercise01;
-
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String args[]) {
+        int foo = 13;
+        System.out.println(foo);
+
+        setFoo(foo);
+        System.out.println(foo);
+    }
+    public static void setFoo(int bar) {
+        bar = 2;
     }
 }
-
