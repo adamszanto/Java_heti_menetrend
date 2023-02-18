071391278385c0d65b76ce6fa4bd62acb6ccb1c9
Adam <adam.szanto1@gmail.com>
2023-02-18 20:02:03 +0100

    2022.02.18.

---
+++ Week_02/Exercises/src/GameWithCharacters/Main.java
@@ -0,0 +1,13 @@
+package GameWithCharacters;
+public class Main {
+    private static final int SIZE = 10;
+    private static final int NUMBER_OF_CHARS = 9;
+    private static final char PLAYER = 'X';
+    private static final char[][] FIELD = new char[SIZE][SIZE];
+
+    private static int playerX;
+    private static int playerY;
+    public static void main(String[] args) {
+        FIELD[0][0] = 2;
+    }
+}
