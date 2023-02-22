e734a7ec3f096d77a952cad6f343b06cef64570b
Adam <adam.szanto1@gmail.com>
2023-02-22 16:19:33 +0100

    2022.02.22.

---
+++ Week_03/Exercises/src/exercise03/Main.java
@@ -0,0 +1,28 @@
+package exercise03;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(divideNums2(3,5));
+        System.out.println(divideNums2(6,2));
+        System.out.println(divideNums2(0,5));
+        System.out.println(divideNums2(5,0));
+        System.out.println(divideNums2(5,1));
+    }
+
+    public static double divideNums(double a, double b) throws ArithmeticException {
+        double result = 0;
+        try {
+            result  = a/b;
+        } catch (ArithmeticException ex) {
+            System.out.println("Can't be divided by Zero " + ex);
+        }
+        return result;
+    }
+
+    public static double divideNums2(double a, double b) throws ArithmeticException {
+        if(b == 0) {
+            throw new ArithmeticException("Can't be divided by Zero");
+        }
+        return a/b;
+    }
+}
---
+++ Week_03/Exercises/src/exercise04/Main.java
@@ -0,0 +1,34 @@
+package exercise04;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        readNumbers();
+    }
+
+    public static void readNumbers() {
+        System.out.println("Give me numbers! Write 0 to stop program");
+        Scanner scanner = new Scanner(System.in);
+        int min = 1;
+        int max = 1;
+        try {
+            while(scanner.nextInt() != 0) {
+                int input = scanner.nextInt();
+                if(input < min) {
+                    min = input;
+                }
+                if(input > max){
+                    max = input;
+                }
+            }
+
+        } catch (NumberFormatException ex) {
+            System.out.println("Number format is incorrect." + ex);
+        } finally {
+            System.out.println("Min: "+ min + "\nMax: " + max);
+        }
+    }
+
+}
