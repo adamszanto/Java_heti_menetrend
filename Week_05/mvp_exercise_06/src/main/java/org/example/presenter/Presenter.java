c027e082a3bf0eb9b80dca0d36e02e7628514747
adamszanto <51482667+adamszanto@users.noreply.github.com>
2023-03-20 20:45:58 +0100

    Delete Week_05/mvp_exercise_06 directory

--- Week_05/mvp_exercise_06/src/main/java/org/example/presenter/Presenter.java
+++
@@ -1,7 +0,0 @@
-package org.example.presenter;
-
-public interface Presenter {
-
-    public void createHero(String name, String power);
-    public int getHeroesSize();
-}
--- Week_05/mvp_exercise_06/src/main/java/org/example/view/View.java
+++
@@ -1,7 +0,0 @@
-package org.example.view;
-
-import org.example.presenter.Presenter;
-
-public interface View {
-    public void setPresenter(Presenter presenter);
-}
