08fc8c8b0fa0e979c65cf1a0e1bc4ae9a19c0209
adamszanto <51482667+adamszanto@users.noreply.github.com>
2023-03-20 20:46:16 +0100

    Delete Week_05/MVP_exercise_02 directory

--- Week_05/MVP_exercise_02/src/main/java/org/example/model/Model.java
+++
@@ -1,9 +0,0 @@
-package org.example.model;
-
-import org.example.presenter.Presenter;
-
-public interface Model {
-    public void setPresenter(Presenter presenter);
-    public void updateModel(String value);
-    public String getValue();
-}
--- Week_05/MVP_exercise_02/src/main/java/org/example/presenter/Presenter.java
+++
@@ -1,6 +0,0 @@
-package org.example.presenter;
-
-public interface Presenter {
-    public void updateValue(String value);
-    public void triggerValueUpdated();
-}
