259bf25f824d0881a9f0380ec03ab4cbc749d03b
adamszanto <51482667+adamszanto@users.noreply.github.com>
2023-03-20 20:46:24 +0100

    Delete Week_05/MVP_exercise_03 directory

--- Week_05/MVP_exercise_03/src/main/java/org/example/model/HeroModel.java
+++
@@ -1,8 +0,0 @@
-package org.example.model;
-
-import java.util.ArrayList;
-
-public interface HeroModel {
-    public void addHero(String name, int power);
-    ArrayList<Hero> getHeroes();
-}
--- Week_05/MVP_exercise_03/src/main/java/org/example/presenter/HeroPresenter.java
+++
@@ -1,6 +0,0 @@
-package org.example.presenter;
-
-public interface HeroPresenter {
-    public void generateHero();
-    public void saveHeroes();
-}
